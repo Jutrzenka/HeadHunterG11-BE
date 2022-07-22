1028fd5c10d9f8d6c03114270ac0b5fe3106be57
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-07-22 17:56:54 +0200

    create remonte clustern in MongoDb. Add url to cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://AlexWilk:alamakota@headhuntercluster.cvbkb.mongodb.net/?retryWrites=true&w=majority',
+      {
+        connectionName: 'headhuntercluster',
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
