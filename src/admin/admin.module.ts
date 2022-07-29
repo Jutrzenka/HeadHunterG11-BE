c4cda53edfb9bccf59e3f65e72b19d242627300a
Kacper Czaja <93550588+Jutrzenka@users.noreply.github.com>
2022-07-29 19:57:46 +0200

    0000 0/osa/admin panel (#14)
    * Generated admin module
    
    * Configuration admin.module.ts
    
    * Added jwt for admin
    
    Co-authored-by: Rafał I <iwanczak.rafal@gmail.com>

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { User, UserSchema } from '../Utils/schema/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtAdminStrategy } from './jwtAdmin.strategy';
+
+@Module({
+  imports: [
+    //add schema for admin
+    // MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [AdminController],
+  providers: [AdminService, JwtAdminStrategy],
+  exports: [JwtAdminStrategy],
+})
+export class AdminModule {}
