7f6f1feed57131d76cab40a7dd799ba19f3824d5
Kacper Czaja <kacperczaja1999@gmail.com>
2022-08-12 18:30:09 +0200

    Added edit-email.template.ts and added send mail in editMail patch

--- src/mail/templates/edit-email.template.ts
+++ src/mail/templates/edit-email.template.ts
@@ -1,10 +1,11 @@
 import configuration from '../../Utils/config/configuration';
 import * as striptags from 'striptags';
 
-export const registerEmailTemplate = (login: string, registerCode: string) => {
+export const editEmailTemplate = (login: string, registerCode: string) => {
   const { ssl, domain, port } = configuration().server;
   return `
   <h1>HeadHunter MegaK</h1>
+  <h2>Email na Twoim koncie został zmieniony. Wejdź w poniższy link, aby dokończyć zmianę.</h2>
   <p>Twój link aktywacyjny do platformy HeadHunter MegaK:</p>
   <a href="${
     ssl ? 'https://' : 'http://'
---
+++ src/mail/templates/export.ts
@@ -0,0 +1,2 @@
+export * from './edit-email.template';
+export * from './register-email.template';
