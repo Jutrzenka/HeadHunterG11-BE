3718b21f5173b729690d395268b05766cb2220ac
Kacper Czaja <kacperczaja1999@gmail.com>
2022-08-12 18:34:41 +0200

    Added newPassword-email.template.ts and added send mail in resetPassword patch

---
+++ src/mail/templates/newPassword-email.template.ts
@@ -0,0 +1,24 @@
+import configuration from '../../Utils/config/configuration';
+import * as striptags from 'striptags';
+
+export const newPasswordEmailTemplate = (
+  login: string,
+  registerCode: string,
+) => {
+  const { ssl, domain, port } = configuration().server;
+  return `
+  <h1>HeadHunter MegaK</h1>
+  <h2>Poprosiłeś o zmianę hasła. Poniżej znajduje się link do zmiany.</h2>
+  <p>Twój link aktywacyjny do platformy HeadHunter MegaK:</p>
+  <a href="${
+    ssl ? 'https://' : 'http://'
+  }register.${domain}:${port}/api/auth/register/${login}/${registerCode}">
+  KLIKNIJ TUTAJ
+</a>
+  <p>Lub wejdź na ten adres: ${
+    ssl ? 'https://' : 'http://'
+  }register.${domain}:${port}/api/auth/register/${striptags(
+    login,
+  )}/${registerCode}</p>
+    `;
+};
